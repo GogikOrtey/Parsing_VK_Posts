95bb01589c1c104830cc31c85210a6d039788ebb
Gogik <gog.ortey@yandex.ru>
2024-05-11 17:00:17 +1000

    Работаю с загрузкой видео - чищу код загрузчика. Он работает

--- video_downloader 2.js
+++ video_downloader 2.js
@@ -8,29 +8,7 @@ import puppeteer from 'puppeteer-core';
 
 
 
-// const browser = await puppeteer.launch({
-//     executablePath: 'C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe', // замените на путь к вашему Edge
-//     headless: false // открывает браузер в режиме с графическим интерфейсом
-// });
-// console.log('Браузер успешно открыт');
-// // Закрываем браузер после 5 секунд
 
-// const page = browser.newPage();
-// page.goto('https://www.downloadvideosfrom.com/ru/VK.php#GoogleBetweenAd');
-
-// setTimeout(() => {
-//     console.log('вставляем текстовую строку в поле ввода с id="url"');
-//     // вставляем текстовую строку в поле ввода с id="url"
-//     page.evaluate(() => {
-//         document.querySelector('#url').value = 'https://vk.com/video-72495085_456242529';
-//     });
-// }, 5000);
-
-// setTimeout(() => {
-//     console.log('инициируем нажатие на кнопку с id="DownloadMP4HD"')
-//     // инициируем нажатие на кнопку с id="DownloadMP4HD"
-//     page.click('#DownloadMP4HD');
-// }, 500);
 
 
 
@@ -71,49 +49,77 @@ import puppeteer from 'puppeteer-core';
 
 
 
-(async () => {
-    try {
-        const browser = await puppeteer.launch({
-            executablePath: 'C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe', // замените на путь к вашему Edge
-            //headless: false // открывает браузер в режиме с графическим интерфейсом
-        });
-        console.log('Браузер успешно открыт');
 
-        const page = await browser.newPage();
 
-        const pagePromise = page.goto('https://www.downloadvideosfrom.com/ru/VK.php#GoogleBetweenAd', 
-        { waitUntil: 'load' });
-        const timeoutPromise = new Promise((resolve) => setTimeout(resolve, 5000));
-    
-        const result = await Promise.race([pagePromise, timeoutPromise]);
-    
-        if (result === pagePromise) {
-          console.log('Страница загружена');
-          // Ваш код здесь...
-        } else {
-          console.log('Время ожидания истекло');
-          // Обработайте ошибку загрузки страницы
-        }
 
-        setTimeout(() => {
-            console.log('вставляем текстовую строку в поле ввода с id="url"');
-            // вставляем текстовую строку в поле ввода с id="url"
-            page.evaluate(() => {
-                document.querySelector('#url').value = 'https://vk.com/video-72495085_456242529';
-            });
 
-            setTimeout(() => {
-                console.log('инициируем нажатие на кнопку с id="DownloadMP4HD"')
-                // инициируем нажатие на кнопку с id="DownloadMP4HD"
-                page.click('#DownloadMP4HD');
-            }, 500);
 
-        }, 5000);
+DownloadVideoFromURL('https://vk.com/video-72495085_456242529')
 
-    } catch (error) {
-        console.error('Произошла Ошибка:', error);
+
+
+
+async function DownloadVideoFromURL(inputURLVideo) {
+  let localMainCounter = 0; // Счётчик, который показывает, какое у нас сейчас состояние в коде
+
+  try {
+    // console.log("inputURLVideo = " + inputURLVideo);
+
+    localMainCounter = 0; console.log(localMainCounter + ': Открываем браузер');
+
+    // Запускаем браузер:
+    const browser = await puppeteer.launch({
+      // Путь к исполняемому файлу браузера Edge
+      executablePath: 'C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe',
+      headless: false // Открывает браузер в режиме с графическим интерфейсом
+    });
+
+    localMainCounter = 1; console.log(localMainCounter + ': Браузер успешно открыт');
+
+    const page = await browser.newPage();
+
+    const pagePromise = page.goto('https://www.downloadvideosfrom.com/ru/VK.php#GoogleBetweenAd',
+      { waitUntil: 'load' });
+    const timeoutPromise = new Promise((resolve) => setTimeout(resolve, 5000));
+
+    localMainCounter = 2; console.log(localMainCounter + ': Ждём 5 секунд, пока страница загрузится');
+
+    const result = await Promise.race([pagePromise, timeoutPromise]);
+    // await Promise.race([pagePromise, timeoutPromise]);
+
+    if (result === pagePromise) {
+      console.log('Страница загружена');
+    } else {
+      console.log('Время ожидания истекло');
     }
-})();
+
+    localMainCounter = 3; console.log(localMainCounter + ': Идём дальше');
+
+    setTimeout(() => {
+      localMainCounter = 4;
+      console.log(localMainCounter + ': Вставляем текстовую строку в поле ввода с id="url"');
+
+      // page.evaluate(() => {
+      //   // document.querySelector('#url').value = 'https://vk.com/video-72495085_456242529';
+      //   document.querySelector('#url').value = inputURLVideo;
+      // });
+
+      page.evaluate((url) => {
+        document.querySelector('#url').value = url;
+      }, inputURLVideo);
+
+      setTimeout(() => {
+        localMainCounter = 5;
+        console.log(localMainCounter + ': Инициируем нажатие на кнопку с id="DownloadMP4HD"');
+        // Инициируем нажатие на кнопку с id="DownloadMP4HD"
+        page.click('#DownloadMP4HD');
+      }, 500);
+    }, 5000);
+
+  } catch (error) {
+    console.error('Произошла Ошибка:', error);
+  }
+}
 
 
 
@@ -126,17 +132,6 @@ import puppeteer from 'puppeteer-core';
 
 
 
-// (async () => {
-//     try {
-//         const browser = await puppeteer.launch({
-//             executablePath: 'C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe', 
-//         });
-//         const page = await browser.newPage();
-//         await page.goto('https://www.downloadvideosfrom.com/ru/VK.php#GoogleBetweenAd'); 
-//     } catch (error) {
-//         console.error('Произошла ошибка:', error);
-//     }
-// })();
 
 
 
@@ -152,30 +147,5 @@ import puppeteer from 'puppeteer-core';
 
 
 
-// (async () => {
-//     const browser = await puppeteer.launch({
-//         executablePath: 'C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe', // замените на путь к вашему Edge
-//     });
-//     const page = await browser.newPage();
-//     await page.goto('https://www.downloadvideosfrom.com/ru/VK.php#GoogleBetweenAd'); 
 
-// //   // ждем, пока страница полностью не загрузится
-// //   await page.waitForNavigation({ waitUntil: 'networkidle0' });
 
-// //   // вставляем текстовую строку в поле ввода с id="url"
-// //   await page.evaluate(() => {
-// //     document.querySelector('#url').value = 'https://vk.com/video-72495085_456242529';
-// //   });
-
-// //   // инициируем нажатие на кнопку с id="DownloadMP4HD"
-// //   await page.click('#DownloadMP4HD');
-
-// //   // ждем, пока файл полностью не загрузится
-// //   // это может быть сложно, так как Puppeteer не предоставляет прямого способа отслеживания загрузки файлов
-// //   // вместо этого, вы можете использовать некоторые обходные пути, например, проверять наличие файла в директории загрузки через некоторые интервалы времени
-
-// //   // написать в консоли сообщение об успешной загрузке
-// //   console.log('Файл успешно загружен');
-
-// //   //await browser.close();
-// })();
